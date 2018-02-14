ad3ac0f23f3987985299d82291071a1882948747
RVR87Programming <programming@rvr87.com>
2018-02-14 18:19:38 -0500

    thing

--- src/org/usfirst/frc/team87/robot/subsystems/LinearLiftSubsystem.java
+++ src/org/usfirst/frc/team87/robot/subsystems/LinearLiftSubsystem.java
@@ -15,8 +15,8 @@ public class LinearLiftSubsystem extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	
-	Talon  leftMotor = new Talon(RobotMap.linearMotorLeft);
-	Talon rightMotor = new Talon(RobotMap.linearMotorRight);
+	Talon  leftMotor = new Talon(RobotMap.LINEARLIFTMOTORLEFT);
+	Talon rightMotor = new Talon(RobotMap.LINEARLIFTMOTORRIGHT);
 	
 	public LinearLiftSubsystem() {
 		
